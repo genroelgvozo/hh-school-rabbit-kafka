006771b7722c6fe8c0bb279947043632f89dda04
Oleg Negrozov <o.negrozov@hh.ru>
2026-04-21 20:51:07 +0300

    fix bitnami

--- kafka_schema_initialize.sh
+++ kafka_schema_initialize.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 docker exec hh-school-rabbit-kafka-kafka-1 \
-  /opt/bitnami/kafka/bin/kafka-topics.sh --create --topic example_topic --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3
+  /opt/kafka/bin/kafka-topics.sh --create --topic example_topic --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3
